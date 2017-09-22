af5b8fa5283ca49cfa6006fedaf5b5af1780506d
JGPNS <PlusNinetySix@users.noreply.github.com>
2017-09-22 13:27:19 -0500

    updated

--- espn.java
+++ espn.java
@@ -6,6 +6,11 @@ public class espn
 {
 	public static void main(String[] args) throws IOException
 	{
+		/**
+		 * 
+		 * @param Scanner Allows for a command to be entered. 
+		 * @param while Allows the user to enter multiple commands until it is terminated. 
+		 */
 		Scanner cin=new Scanner(System.in);
 		System.out.println("Enter a command");
 		String command;
@@ -13,35 +18,57 @@ public class espn
 		gradebook g = null;
 		while(true)
                 {
-                   command = cin.nextLine();
+					
+                   command = cin.nextLine(); //User can read in commands.
+                 
                    if(command.equalsIgnoreCase("Read"))
-                   {
+                   {//Reads the grades into an array.
+                	   /**
+                        * Allows the user to type the read command and enter the name of the gradebook file.
+                        */
                 	   System.out.println("Please enter the name of the gradebook file.");
                 	   String filename = cin.nextLine();
                 	   g=new gradebook(filename);
                    }
+                   
                     if(command.equalsIgnoreCase("Max"))
-                    {
+                    {//Outputs the highest grade.
+                    	/**
+                         * Allows the user to view the highest grade.
+                         */
                         System.out.println(g.Max());
                         System.out.println("Enter another command.");
                     }
+                    
                     if(command.equalsIgnoreCase("Min"))
-                    {
+                    {//Outputs the lowest grade.
+                    	/**
+                         * Allows the user to view the lowest grade.
+                         */
                         System.out.println(g.Min());
                         System.out.println("Enter another command.");
                     }
+                    
                     if(command.equalsIgnoreCase("Average"))
-                    {
+                    {//Outputs the average grade.
                         System.out.println(g.Mean());
                         System.out.println("Enter another command.");
+                        /**
+                         * Allows the user to view the average grade.
+                         */
                     }
+                    
                     if(command.equalsIgnoreCase("Median"))
-                    {
+                    {//Outputs the middle-most grade.
                         System.out.println(g.Median());
                         System.out.println("Enter another command.");
+                        /**
+                         * Allows the user to view the middle-most grade.
+                         */
                     }
+                    
                     if(command.equalsIgnoreCase("Distro"))
-                    {
+                    {//Outputs the grade distrobution.
                         int [] distro = g.Distrobution();
                 		System.out.println("You have "+ distro[4] +" F(s)");
                 		System.out.println("You have "+ distro[3] +" D(s)");
@@ -49,23 +76,38 @@ public class espn
                 		System.out.println("You have "+ distro[1] +" B(s)");
                 		System.out.println("You have "+ distro[0] +" A(s)");
                 		System.out.println("Enter another command.");
+                		/**
+                         * Allows the user to view the grade distrobution.
+                         */
                     }
+                    
                     if(command.equalsIgnoreCase("Count"))
-                    {
+                    {//Outputs the number of grades.
                     	System.out.println("There are "+g.grades.length+" grades in this gradebook.");
+                    	/**
+                         * Allows the user to view how many grades are in the gradebook.
+                         */
                     }
+                    
                     if(command.equalsIgnoreCase("Empty"))
-                    {
+                    {//Clears the array.
                     	g=null;
                     	System.out.println("Emptied gradebook.");
                     	System.out.println("Enter read command, then the filename to continue.");
+                    	/**
+                         * Allows the user to empty the gradebook and put in the name of a new gradebook.
+                         */
                     }
+                    
                     if(command.equalsIgnoreCase("Exit"))
-                    {
+                    {//Quits the program.
                     	System.out.println("Goodbye!");
                     	break;
+                    	/**
+                         * Allows the user to exit the program.
+                         */
                     }
                 }
-		cin.close();
+		cin.close(); //Closes cin.
 	}
 }
--- gradebook.java
+++ gradebook.java
@@ -7,19 +7,28 @@ import java.io.IOException;
 public class gradebook
 {
 	int [] grades;
-	
+	/**
+	 * Lowest grade.
+	 * @return Returns the lowest grade in the gradebook.
+	 */
 	int Min() 
-	{
+	{//Outputs the lowest grade.
 		return grades[0];
 	}
-	
+	/**
+	 * Highest grade.
+	 * @return Returns the highest grade in the gradebook.
+	 */
 	int Max()
-	{
+	{//Outputs the highest grade.
 		return grades[grades.length-1];
 	}
-	
+	/**
+	 * Average grade.
+	 * @return Returns the average grade for the gradebook.
+	 */
 	double Mean()
-	{
+	{//Calulates average of the grades.
 		{
 		double sum = 0;
 			for(int i = 0; i<grades.length; i++)
@@ -30,9 +39,14 @@ public class gradebook
 				return sum;
 		}
 	}
-	
+	/**
+	 * Middle-most grade.
+	 * @return Returns the middle-most grade.
+	 * @param if Incase there is an even number of grades.
+	 * @param else Incase there is an odd number of grades.
+	 */
 	double Median()
-	{
+	{//First is if even grades, second is if odd.
 		double median;
 		if (grades.length % 2 == 0)
 			median = ((double)grades[grades.length/2]+(double)grades[grades.length/2 - 1])/2;
@@ -40,9 +54,12 @@ public class gradebook
 			median = (double) grades[grades.length/2];
 				return median;
 	}
-	
+	/**
+	 * Grade distrobution.
+	 * @return How many of each letter grade there is.
+	 */
 	int[] Distrobution()
-	{
+	{//Puts grades into different categories.
 		int [] distro = {0,0,0,0,0};
 		for(int i = 0; i<grades.length; i++) 
 		{
@@ -54,9 +71,13 @@ public class gradebook
 		}
 		return distro;
 	}
-	
+	/**
+	 * Main function.
+	 * @param filename Asks for the filename.
+	 * @throws IOException throws an IOException if file is not found.
+	 */
 	public gradebook(String filename) throws IOException
-	{
+	{//Main function. Reads grades into an array.
 		Scanner fin=new Scanner(new FileReader(filename));
 			int n=fin.nextInt();
 				grades=new int[n];
